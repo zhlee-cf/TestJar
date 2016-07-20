14f8e591ab9993be77692c09d7fb2572c134eb52
zhlee_cf <1281861976@qq.com>
2016-07-20 14:43:49 +0800

    Initial commit

---
+++ app/src/main/java/com/im/testjar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.im.testjar;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+import com.im.openimlib.app.OpenIM;
+import com.im.openimlib.fragment.ConversationListFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OpenIM.init(this, "lizhcf", "123123");
+        initView();
+    }
+    private void initView() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.main_frame, new ConversationListFragment()).commit();
+    }
+}
---
+++ app/src/main/java/com/im/testjar/MyApp.java
@@ -0,0 +1,9 @@
+package com.im.testjar;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by lzh12 on 2016/7/20.
+ */
+public class MyApp extends LitePalApplication {
+}
